ee7dd5981d5b0b1401dbf4e24eb365c229985a8e
haard18 <haardsolanki.itm@gmail.com>
2024-08-07 17:18:17 +0530

    button fix

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,22 +1,15 @@
-import { useNavigate } from "react-router-dom"
+// import { useNavigate } from "react-router-dom"
 import { TypewriterEffectSmoothDemo } from "./Typewriter";
 
 const Home = () => {
-    const navigate = useNavigate();
-    const handleClick = () => {
-        navigate('/predict')
-    }
-    const handleClick2 = () => {
-        navigate('/upload')
-    }
+    
     return (
 
         <div className="flex gap-4 flex-col">
 
             <div>
                 <TypewriterEffectSmoothDemo/>
-                <button onClick={handleClick} className="btn btn-outline btn-primary">Go to Predict</button>
-                <button onClick={handleClick2} className="btn btn-outline btn-primary">Upload your custom Data</button>
+                
             </div>
 
         </div>
